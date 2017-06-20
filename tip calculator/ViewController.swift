f510ead087b20618e55292420cdd91516412c25d
Cameryn Boyd <cboyd7@fb.com>
2017-06-20 15:27:37 -0700

    Added tap gesture recognition + made a numeric keyboard

--- tip calculator/ViewController.swift
+++ tip calculator/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
 
 }
 
